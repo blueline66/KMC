1a44c8f0b2c4373e5dead99ce40c8fd31723a699
blueline66 <darinazabcik980@gmail.com>
2026-09-22 22:08:19 +0300

    лекція 2

--- lab1/obj/Debug/net10.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net10.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5226b07b9aee32a47b6d46b2ddcf68833ee2aee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b0135fc1266e26aa79f3000714c36a8a156fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
